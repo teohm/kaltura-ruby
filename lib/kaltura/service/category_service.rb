93d6ea8fd9ee6e576c2b34555037018972dee451
patricksrobertson <patricksrobertson@gmail.com>
2010-09-27 19:06:29 -0400

    completed documentation for BaseEntryService, BaseService, BulkUploadService, CategoryService, ConversionProfileService, DataService

--- lib/kaltura/service/category_service.rb
+++ lib/kaltura/service/category_service.rb
@@ -1,19 +1,74 @@
 module Kaltura
   module Service
+    ##
+    # CategoryService is responsible for adding and managing Categories.  Categories are the main 
+    # means to filter entries within the KMC.
+    #
+    # @example Add a new category to the KMC filter list.
+    #   new_category = Kaltura::Category.new
+    #   new_category.name = "waffles"
+    #   client.category_service.add(new_category)
+    #
+    # @example Get an existing category by ID.
+    #   client.category_service.get(214)
+    #
+    # @example Update an existing category.
+    #   category_update = Kaltura::Category.new
+    #   category_update.description = "waffles are pretty freaking good."
+    #   client.category_service.update(214,category_update)
+    #
+    # @example Delete an existing category.
+    #   client.category_service.delete(214)
+    #
+    # @example List all categories with a parent_id of 2
+    #   category_filter = Kaltura::Filter::CategoryFilter.new
+    #   category_filter.parent_id_equal = 2
+    #   client.category_service.list(category_filter)
+    ##
     class CategoryService < BaseService
-
+      
+      ##
+      # Adds a new Category.
+      #
+      # @param [Kaltura::Category] category The category that will be created.  The crucial field to fill out is name.
+      # 
+      # @return [Kaltura::Category] Returns the category as it now appears on the server.  It would be beneficial to 
+      #   save the ID of the category for later reference.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def add(category)
   			kparams = {}
   			client.add_param(kparams, 'category', category)
   			perform_request('category','add',kparams,false)
   		end
 
+      ##
+      # Retrieves a category by it's ID.
+      #
+      # @param [Integer] id The Kaltura::Category object's id.
+      #
+      # @return [Kaltura::Category] The requested category.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def get(id)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
   			perform_request('category','get',kparams,false)
   		end
 
+      ##
+      # Updates a category.
+      # The best practice as with all updates would be to insantiate a new Kaltura::Category and update specific fields.
+      #
+      # @param [Integer] id The Kaltura::Category id of the category to be updated.
+      # @param [Kaltura::Category] A newly insantiated Category object with the fields updated that you'd like to update.
+      # 
+      # @return [Kaltura::Category] The new and improved Category object.
+      # 
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def update(id, category)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
@@ -21,12 +76,32 @@ module Kaltura
   			perform_request('category','update',kparams,false)
   		end
 
+      ##
+      # Deletes the requested Category.
+      # I am fairly confident that this will not actually clear the category from each entry, but it is no longer
+      # a filter on the KMC.
+      #
+      # @param [Integer] id The category ID to remove.
+      #
+      # @return [nil] Returns nothing.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def delete(id)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
   			perform_request('category','delete',kparams,false)
   		end
 
+      ##
+      # Lists all Categories given the specified filter.
+      #
+      # @param [Kaltura::Filter::CategoryFilter] filter The filter to apply.
+      #
+      # @return [Kaltura::Response::CategoryListResponse] Wrapper for Kaltura::Response::BaseResponse.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.      
+      ##
   		def list(filter=nil)
   			kparams = {}
   			client.add_param(kparams, 'filter', filter)
--- lib/kaltura/service/conversion_profile_service.rb
+++ lib/kaltura/service/conversion_profile_service.rb
@@ -1,19 +1,78 @@
 module Kaltura
   module Service
+    
+    ##
+    # The Conversion Profile Service is responsible for adding and managing conversion profiles.
+    #
+    # @example Create a new conversion profile:
+    #   conversion_profile = Kaltura::ConversionProfile.new
+    #   conversion_profile.name = "waffles"
+    #   conversion_profile.is_default = "true"
+    #   conversion_profile.clip_duration = 15
+    #   client.conversion_profile_service.add(conversion_profile)
+    #
+    # @example Retrieve a conversion profile:
+    #   client.conversion_profile_service.get(224)
+    #
+    # @example Update a conversion profile:
+    #   update_profile = Kaltura::ConversionProfile.new
+    #   update_profile.description = "why does the documentor love waffles so much?"
+    #   update_profile.clip_start = 3
+    #   client.conversion_profile_service.update(224,update_profile)
+    #
+    # @example Delete a conversion profile:
+    #   client.conversion_profile_service.delete(224)
+    #
+    # @example Retrieve all conversion profiles with an ID of 224
+    #   filter = Kaltura::Filter::ConversionProfileFilter.new
+    #   filter.id_equal = 224
+    #   client.conversion_profile_service.list(filter)
+    ##
     class ConversionProfileService < BaseService
 
+      ##
+      # Adds a new conversion profile.
+      #
+      # @param [Kaltura::ConversionProfile] conversion_profile The new conversion profile.
+      #
+      # @return [Kaltura::ConversionProfile] The conversion profile as it is stored on the Kaltura server.
+      #   It might be important to store the ID returned.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def add(conversion_profile)
   			kparams = {}
   			client.add_param(kparams, 'conversionProfile', conversion_profile)
   			perform_request('conversionProfile','add',kparams,false)
   		end
-
+      
+      ##
+      # Retrieves a conversion profile by ID.
+      #
+      # @param [Integer] id The conversion profile ID.
+      #
+      # @return [Kaltura::ConversionProfile] The requested conversion profile.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##      
   		def get(id)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
   			perform_request('conversionProfile','get',kparams,false)
   		end
 
+      ##
+      # Updates a conversion profile given an ID and a newly instantiated ConversionProfile.
+      # It will update any populated fields on the provided profile.
+      #
+      # @param [Integer] id The conversion profile ID.
+      # @param [Kaltura::ConversionProfile] conversion_profile Newly instantiated conversion profile with 
+      #   the fields to update.
+      # 
+      # @return [Kaltura::ConversionProfile] Returns an updated conversion profile.
+      # 
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def update(id, conversion_profile)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
@@ -21,12 +80,31 @@ module Kaltura
   			perform_request('conversionProfile','update',kparams,false)
   		end
 
+      ##
+      # Deletes a conversion profile by ID.
+      #
+      # @param [Integer] id The conversion profile ID.
+      #
+      # @return [nil] Returns nothing.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def delete(id)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
   			perform_request('conversionProfile','delete',kparams,false)
   		end
 
+      ##
+      # Lists conversion profiles given a filter with paging support for larger result sets.
+      #
+      # @param [Kaltura::Filter::ConversionProfileFilter] filter The filter to apply to the list.
+      # @param [Kaltura::FilterPager] pager Default Kaltura Pager.
+      #
+      # @return [Kaltura::Response::ConversionProfileListResponse] Wrapper for Kaltura::Response::BaseResponse.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def list(filter=nil, pager=nil)
   			kparams = {}
   			client.add_param(kparams, 'filter', filter)
