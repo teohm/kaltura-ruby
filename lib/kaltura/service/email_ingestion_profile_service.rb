2c358da2f5641678cf492ba1ff1c736e00b52b34
patricksrobertson <patricksrobertson@gmail.com>
2010-09-28 13:06:16 -0400

    added email profile and flavor asset services to the documentation

--- lib/kaltura/service/email_ingestion_profile_service.rb
+++ lib/kaltura/service/email_ingestion_profile_service.rb
@@ -1,25 +1,73 @@
 module Kaltura
   module Service
+    ##
+    # The Email Ingestion Profile Service allows you to manage email ingestion profile records.
+    ##
     class EmailIngestionProfileService < BaseService
-
+      
+      ##
+      # Adds an email ingestion profile to the Kaltura Database.
+      #
+      # @param [Kaltura::EmailIngestionProfile] email_ip The email ingestion profile to add.
+      #
+      # @return [Kaltura::EmailIngestionProfile] Returns the email ingestion profile as stored on the DB.
+      #   It might be important to store either the email_address or ID fields for later.
+      #
+      # @raise [Kaltura::APIError] Raises 'EMAIL INGESTION PROFILE EMAIL EXISTS' if there is another profile using
+      #   the email address provided.
+      ##
   		def add(email_ip)
   			kparams = {}
   			client.add_param(kparams, 'EmailIP', email_ip)
   			perform_request('EmailIngestionProfile','add',kparams,false)
   		end
-
+      
+      ##
+      # Retrieves an email ingestion profile by the email_address field.
+      #
+      # @param [String] email_address The email address to find the profile with.
+      #
+      # @return [Kaltura::EmailIngestionProfile] The requested email ingestion profile.
+      #
+      # @raise [Kaltura::APIError] Raises 'EMAIL INGESTION PROFILE NOT FOUND' if there isnt a profile with the
+      #   email address provided.
+      ##
   		def get_by_email_address(email_address)
   			kparams = {}
   			client.add_param(kparams, 'emailAddress', email_address)
   			perform_request('EmailIngestionProfile','getByEmailAddress',kparams,false)
   		end
 
+      ##
+      # Retrieves an email ingestion profile by ID.
+      #
+      # @param [Integer] id The ID of the email ingestion profile to find.
+      #
+      # @return [Kaltura::EmailIngestionProfile] The requested email ingestion profile.
+      #
+      # @raise [Kaltura::APIError] Raises 'EMAIL INGESTION PROFILE NOT FOUND' if there isnt a profile with the
+      #   email address provided.
+      ##
   		def get(id)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
   			perform_request('EmailIngestionProfile','get',kparams,false)
   		end
 
+      ##
+      # Updates an existing email ingestion profile.
+      # As with other update API actions it is a best practice to instantiate a new EmailIngestionProfile object
+      # and populate the fields with the fields you wish to change of the existing object.
+      #
+      # @param [Integer] id The ID of the email ingestion profile you wish to update.
+      # @param [Kaltura::EmailIngestionProfile] email_ip The newly instantiated email ingestion profile object
+      #  with the fields you wish to update.
+      #
+      # @return [Kaltura::EmailIngestionProfile] Returns the updated object with a new version.
+      #
+      # @raise [Kaltura::APIError] Raises 'EMAIL INGESTION PROFILE NOT FOUND' if there isnt a profile with the
+      #   email address provided.      
+      ##
   		def update(id, email_ip)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
@@ -27,12 +75,39 @@ module Kaltura
   			perform_request('EmailIngestionProfile','update',kparams,false)
   		end
 
+      ##
+      # Deletes an email ingestion profile by ID.
+      #
+      # @param [Integer] id The ID of the email ingestion profile to delete.
+      # 
+      # @return [nil] Returns nothing.
+      #
+      # @raise [Kaltura::APIError] Raises 'EMAIL INGESTION PROFILE NOT FOUND' if there isnt a profile with the
+      #   email address provided.
+      ##
   		def delete(id)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
   			perform_request('EmailIngestionProfile','delete',kparams,false)
   		end
 
+      ##
+      # Adds a Kaltura::MediaEntry from a email ingestion profile.
+      # I'm not entirely sure what the aim of this upload action is, rather than 
+      # using the media service itself.
+      #
+      # @param [Kaltura::MediaEntry] media_entry A newly instantiated media entry object that will be created
+      #   in the database.
+      # @param [String] upload_token_id The upload token provided by the media service's upload action.
+      # @param [Integer] email_profile_id The email ingestion profile ID.
+      # @param [String] from_address The email address the action is coming from?
+      # @param [String] email_msg_id The ID of the email message?
+      # 
+      # @return [Kaltura::MediaEntry] The newly created media entry as stored in the kaltura database.
+      #   It would be beneficial to store the entry_id of this object for later.
+      #
+      # @raise [Kaltura::APIError] Raises 'UPLOADED_FILE_NOT_FOUND_BY_TOKEN' when the upload token cannot be found.
+      ##
   		def add_media_entry(media_entry, upload_token_id, email_prof_id, from_address, email_msg_id)
   			kparams = {}
   			client.add_param(kparams, 'mediaEntry', media_entry)
--- lib/kaltura/service/flavor_asset_service.rb
+++ lib/kaltura/service/flavor_asset_service.rb
@@ -1,25 +1,69 @@
 module Kaltura
   module Service
+    
+    ##
+    # The Flavor Asset Service allows you to retrieve information and invoke actions on flavor assets.
+    #
+    #
+    ##
     class FlavorAssetService < BaseService
 
+      ##
+      # Retrieves an individual flavor asset by ID.
+      #
+      # @param [String] id A flavor asset ID closely resembles an entry_id as Kaltura flavors are 
+      #   very close in nature to entries.
+      #
+      # @return [Kaltura::FlavorAsset] Returns the requested flavor asset.
+      #
+      # @raise [Kaltura::APIError] Raises the default Kaltura errors.
+      ##
   		def get(id)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
   			perform_request('flavorAsset','get',kparams,false)
   		end
 
+      ##
+      # Retrieves an enumerable collection of flavor assets given a media entry id.
+      #
+      # @param [String] entry_id The media entry ID to seek flavors for.
+      #
+      # @return [Array] Returns an array of the flavor assets for a given media entry.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def get_by_entry_id(entry_id)
   			kparams = {}
   			client.add_param(kparams, 'entryId', entry_id)
   			perform_request('flavorAsset','getByEntryId',kparams,false)
   		end
-
+  		
+      ##
+      # Retrieves an enumerable collection of flavor assets that are playable within the KDP for a given media entry ID.
+      #
+      # @param [String] entry_id The media entry ID to seek flavors for.
+      #
+      # @return [Array] Returns an array of the flavor assets for a given media entry.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.      
+      ##
   		def get_web_playable_by_entry_id(entry_id)
   			kparams = {}
   			client.add_param(kparams, 'entryId', entry_id)
   			perform_request('flavorAsset','getWebPlayableByEntryId',kparams,false)
   		end
 
+      ##
+      # Add and convert a new flavor asset for a specified entry given a flavor param.
+      #
+      # @param [String] entry_id The media entry ID to add a new flavor asset to.
+      # @param [Integer] flavor_params_id The flavor params ID for conversion type.
+      #
+      # @return [nil] Returns nothing.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def convert(entry_id, flavor_params_id)
   			kparams = {}
   			client.add_param(kparams, 'entryId', entry_id)
@@ -27,24 +71,60 @@ module Kaltura
   			perform_request('flavorAsset','convert',kparams,false)
   		end
 
+      ##
+      # Reconvert a specific flavor asset.
+      #
+      # @param [String] id The flavor asset id.
+      # 
+      # @return [nil] Returns nothing.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def reconvert(id)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
   			perform_request('flavorAsset','reconvert',kparams,false)
   		end
 
+      ##
+      # Removes a flavor asset.
+      #
+      # @param [String] id Flavor asset to remove.
+      #
+      # @return [nil] Returns nothing.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def delete(id)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
   			perform_request('flavorAsset','delete',kparams,false)
   		end
 
+      ##
+      # Gets a downloadable URL for a flavor asset.
+      #
+      # @param [String] id The flavor asset id.
+      #
+      # @return [String] a URL that can be used to download the requested flavor.
+      #
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def get_download_url(id)
   			kparams = {}
   			client.add_param(kparams, 'id', id)
   			perform_request('flavorAsset','getDownloadUrl',kparams,false)
   		end
-
+  		
+      ##
+      # Retrieves an enumerable collection of Flavor Assets + Flavor Params given a media entry.
+      #
+      # @param [String] entry_id The media entry to retrieve flavors from.
+      #
+      # @return [Array] Returns a collection of Flavor Assets + Flavor Params.
+      # 
+      # @raise [Kaltura::APIError] Raises default Kaltura errors.
+      ##
   		def get_flavor_assets_with_params(entry_id)
   			kparams = {}
   			client.add_param(kparams, 'entryId', entry_id)
